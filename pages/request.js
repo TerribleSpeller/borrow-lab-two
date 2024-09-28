e83bfc52be631b5c64a1e99d3e96313c9fb453f3
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2024-09-28 17:27:55 +0700

    Find User

--- pages/request.js
+++ pages/request.js
@@ -34,7 +34,7 @@ function Request() {
     const router = useRouter();
     const { id, name } = router.query;
     const [equipment, setEquipment] = useState([]);
-    const [selectedEquipment, setSelectedEquipment] = useState(id ? [id]: []);
+    const [selectedEquipment, setSelectedEquipment] = useState(id ? [id] : []);
     const [searchQuery, setSearchQuery] = useState("");
     const [labOptions, setLabOptions] = useState([]);
     const [availableQty, setAvailableQty] = useState(0);
@@ -353,37 +353,37 @@ function Request() {
                         <h3>Peraturan Peminjaman Barang Laboratorium:</h3>
                         <p>
                             <strong>Penggunaan Dalam Kampus</strong>
-                            <br/>
+                            <br />
                             Barang laboratorium hanya diperbolehkan digunakan di area kampus.
                         </p>
                         <p>
                             <strong>Durasi Peminjaman</strong>
-                            <br/>
+                            <br />
                             Peminjaman barang laboratorium disarankan untuk jangka waktu 1 hari. Jika memerlukan waktu lebih dari 1 hari, maka peminjam harus berkoordinasi terlebih dahulu dengan dosen pembimbing.
                         </p>
                         <p>
                             <strong>Pengajuan Peminjaman Jangka Panjang</strong>
-                            <br/>
+                            <br />
                             Jika dosen pembimbing mengizinkan peminjaman jangka panjang, maka peminjam wajib mengirimkan email kepada kepala laboratorium  (benedictus.rahardjo@binus.edu) dan cc kepala program untuk diketahui bersama-sama.
                         </p>
                         <p>
                             <strong>Batas Maksimal Peminjaman</strong>
-                            <br/>
+                            <br />
                             Batas maksimal peminjaman barang laboratorium adalah 1 bulan .
                         </p>
                         <p>
                             <strong>Perpanjangan Peminjaman</strong>
-                            <br/>
+                            <br />
                             Setelah 1 bulan, jika peminjam masih memerlukan barang yang dipinjam, maka peminjam perlu mengajukan permohonan ulang peminjaman dari proses awal.
                         </p>
                         <p>
                             <strong>Pengembalian Barang Pinjaman</strong>
-                            <br/>
+                            <br />
                             Peminjam wajib mengembalikan barang yang dipinjam kepada laboran secara langsung.
                         </p>
                         <p>
                             <strong>Tanggung Jawab atas Kerusakan atau Kehilangan</strong>
-                            <br/>
+                            <br />
                             Jika peminjam merusak atau menghilangkan barang laboratorium, peminjam wajib mengganti rugi senilai harga barang tersebut atau mengganti barang baru dengan tipe dan spesifikasi yang sama persis.
                         </p>
                         <p>
@@ -395,7 +395,7 @@ function Request() {
                         <p>
                             Laboratorium
                         </p>
-                    </>                    
+                    </>
                     <Form.Check
                         type="checkbox"
                         label="Saya bersetuju dengan peraturan yang diberikan"
